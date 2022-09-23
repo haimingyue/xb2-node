21348ad4c18a86e12ed66124d5120ddb5b23ebb4
simoon <420526391@qq.com>
2022-09-24 06:35:46 +0800

    express

--- src/main.js
+++ src/main.js
@@ -1,17 +1,30 @@
-const http = require('http');
-
-const server = http.createServer((request, response) => {
-    const data = {
-        id: 1
-    }
-    const jsonData = JSON.stringify(data)
-    response.writeHead(200, {
-        'Content-Type': 'application/json; chatset="utf-8'
-    })
-    response.write(jsonData);
-    response.end();
+const express = require('express');
+const app = express();
+const port = 3000;
+app.listen(port, () => {
+	console.log('服务已启动');
 });
 
-server.listen(3000, () => { 
-    console.log('服务已已启动');
-})
+app.get('/', (repuset, response) => {
+	response.send('你好');
+});
+
+const data = [
+	{
+		id: 1,
+		title: '忆江南'
+	},
+    {
+		id: 2,
+		title: '千与千寻'
+	}
+];
+
+app.get('/posts', (req, res) => {
+	res.send(data);
+});
+app.get('/posts/:postId', (req, res) => {
+	let { postId } = req.params;
+    let d = data.filter(i => i.id == postId)[0]
+	res.send(d);
+});
