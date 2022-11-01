f0ae76ee3a437fea7066ccc93277dc809f040f2b
simoon <420526391@qq.com>
2022-11-02 06:29:53 +0800

    定义存储标签的功能

--- src/tag/tag.service.ts
+++ src/tag/tag.service.ts
@@ -1,1 +1,19 @@
 import { connection } from '../app/database/mysql';
+import { TagModel } from './tag.model';
+
+/**
+ * 创建标签
+ */
+
+export const createTag = async (tag: TagModel) => {
+  // 准备查询sql
+  const statement = `
+        INSERT INTO tag
+        SET ?
+    `;
+
+  // 执行查询
+  const [data] = connection.promise().query(statement, tag);
+  // 返回数据
+  return data as any;
+};
